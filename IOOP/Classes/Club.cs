b8e785173d5b9cfb3ae089e26d9f3a40752fec24
DE SILVA RAHAL <TP053533@mail.apu.edu.my>
2020-09-12 22:55:33 +0800

    added classes

---
+++ IOOP/Classes/Club.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOOP_Assignment_Group_11.Classes
+{
+    class Club
+    {
+        private string clubName;
+        private string clubRegDate;
+        private string clubDesc;
+        private string clubPres;
+        private string clubVPres;
+        private string clubSec;
+
+        public Club(string n, string rd, string d, string p, string vp, string s)
+        {
+            clubName = n;
+            clubRegDate = rd;
+            clubDesc = d;
+            clubPres = p;
+            clubVPres = vp;
+            clubSec = s;
+        }
+
+        public void UpdateClubDesc(string d)
+        {
+            clubDesc = d;
+        }
+
+
+    }
+}

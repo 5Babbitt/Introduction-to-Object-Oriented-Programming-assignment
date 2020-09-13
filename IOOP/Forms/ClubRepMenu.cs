91b27259de52977e215468023de180a658571db1
OWEN HARBERT <TP058238@mail.apu.edu.my>
2020-09-13 16:57:36 +0800

    Activity Class

--- IOOP/Forms/ClubRepMenu.cs
+++ IOOP/Forms/ClubRepMenu.cs
@@ -34,7 +34,7 @@ namespace IOOP.Forms
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            WeeklyLog log = new WeeklyLog();
+            WeeklyLog log = new WeeklyLog(CName);
             log.ShowDialog();
 
         }
--- IOOP/Forms/WeeklyLog.cs
+++ IOOP/Forms/WeeklyLog.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using IOOP.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +15,44 @@ namespace IOOP.Forms
 {
     public partial class WeeklyLog : Form
     {
-        public WeeklyLog()
+        public static string cName;
+
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myCS"].ToString());
+
+        public WeeklyLog(string n)
         {
             InitializeComponent();
+            cName = n;
+
+            lblClubName.Text = cName;
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            if(txtDescription.TextLength > 0)
+            {
+                con.Open();
+                Activity activ = new Activity(cName, dateOfEntry.Value, txtDescription.Text);
 
+                SqlCommand cmd = new SqlCommand("insert into Activity(clubName, dateTime, description) values(@name, @doe, @desc)", con);
+
+                cmd.Parameters.AddWithValue("@name", activ.ClubName);
+                cmd.Parameters.AddWithValue("@doe", activ.DateEntry);
+                cmd.Parameters.AddWithValue("@desc", activ.Description);
+
+                int i = cmd.ExecuteNonQuery();
+
+                if (i != 0)
+                    MessageBox.Show("Activity Added Successfully");
+                else
+                    MessageBox.Show("Unable to add activity");
+
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please don't leave any empty fields");
+            }
         }
     }
 }
